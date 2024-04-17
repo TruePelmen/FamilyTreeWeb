997e7bb29d71368005de6c45c73b2ac880d781c6
TruePelmen <oleksii.senyshyn@lnu.edu.ua>
2024-04-17 09:39:03 +0300

    Added AccountController and Register/Login views
    Added AccountController and Register/Login views

---
+++ FamilyTreeWeb/FamilyTreeWeb/Controllers/AccountController .cs
@@ -0,0 +1,55 @@
+﻿using FamilyTreeWeb.Context;
+using FamilyTreeWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyTreeWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly FamilyTreeDbContext _context;
+
+        public AccountController(FamilyTreeDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(newUser);
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var existingUser = _context.Users.FirstOrDefault(u => u.Login == user.Login && u.Password == user.Password);
+            if (existingUser != null)
+            {
+                // Логіка для успішного входу, наприклад, збереження інформації про користувача в сеансі (Session)
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt. Please check your username and password.");
+                return View(user);
+            }
+        }
+    }
+}
--- FamilyTreeWeb/FamilyTreeWeb/obj/Debug/net8.0/FamilyTreeWeb.AssemblyInfo.cs
+++ FamilyTreeWeb/FamilyTreeWeb/obj/Debug/net8.0/FamilyTreeWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamilyTreeWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f0b3427d7439936a8b1cf36449d3e23134a6fed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1e8c51fda23717e26b85145a2db853929c8086a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamilyTreeWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamilyTreeWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
