7e11a3705a2273bbe9db97eaba628dc3478f3a87
TruePelmen <99765351+TruePelmen@users.noreply.github.com>
2024-05-21 18:34:52 +0300

    Update Program.cs

--- FamilyTreeWeb/Program.cs
+++ FamilyTreeWeb/Program.cs
@@ -31,7 +31,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Tree}/{action=Create}");
-    //pattern: "{controller=Home}/{action=Index}/{id?}");
+    //pattern: "{controller=Tree}/{action=Create}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
